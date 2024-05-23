b07c773b06a609fa1ff0fa8737d15cbcd3d9bfec
ChristianJesusC <jesuschiu505@gmail.com>
2024-05-23 06:23:39 -0600

    websocket

--- app.ts
+++ app.ts
@@ -1,104 +1,103 @@
-import express from "express"
-import cors from "cors"
-import http from "http"
-import morgan from "morgan"
-import dotenv from "dotenv"
-import signale from "signale"
-import { WebSocketServer, WebSocket } from "ws"
-import { EventAttributes } from "./types/socketTypes"
-import puntajesModel from "./models/puntajesModel"
-import { IPuntaje } from "./types/IPuntajes"
-import { publicacionesRouter } from "./routes/publicacionesRouter"
-import { usuarioRouter } from "./routes/usuarioRouter"
-import { agendaRouter } from "./routes/agendaRouter"
+import express from "express";
+import cors from "cors";
+import http from "http";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import signale from "signale";
+import { WebSocketServer, WebSocket } from "ws";
+import { EventAttributes } from "./types/socketTypes";
+import puntajesModel from "./models/puntajesModel";
+import { IPuntaje } from "./types/IPuntajes";
+import { publicacionesRouter } from "./routes/publicacionesRouter";
+import { usuarioRouter } from "./routes/usuarioRouter";
+import { agendaRouter } from "./routes/agendaRouter";
 
-dotenv.config()
+dotenv.config();
 
-import connectToDatabase from "./database/database"
+import connectToDatabase from "./database/database";
 const corsOptions = {
-    origin: '*'
-}
+  origin: "*",
+};
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors(corsOptions));
 app.use(morgan("dev"));
 
-app.use('/publicacion', publicacionesRouter);
-app.use('/usuario', usuarioRouter);
-app.use('/agenda', agendaRouter);
+app.use("/publicacion", publicacionesRouter);
+app.use("/usuario", usuarioRouter);
+app.use("/agenda", agendaRouter);
 
 const server = http.createServer(app);
 
 const wss = new WebSocketServer({ server: server });
 
-wss.on('connection', function (socket: WebSocket) {
-    console.log("nueva conexion");
+wss.on("connection", function (socket: WebSocket) {
+  console.log("nueva conexion");
 
-    socket.on('error', function (error: Error) {
-        console.log('ha ocurrido un error.', error.message);
-    });
-    
+  socket.on("error", function (error: Error) {
+    console.log("ha ocurrido un error.", error.message);
+  });
 
-    socket.onmessage = async(event) => {
-        const data = event.data;
-        const parsedData: EventAttributes = JSON.parse(data.toString());
-        const action = parsedData.action;
-        const body = parsedData.body;
-        let puntajes:IPuntaje[] 
-        switch (action) {
-            case "getPuntajes":
-                puntajes = await puntajesModel.find();
-                if (!puntajes) {
-                    socket.send(JSON.stringify({
-                        key: "puntajes",
-                        data: ["no hay puntajes"]
-                    }));
-                }
-                if (puntajes.length > 0) {
-                    puntajes = puntajes.sort((a, b) => b.puntaje - a.puntaje);
-                }
-                console.log("puntajes ordenados, punto final 2")
-                socket.send(JSON.stringify({
-                    key: "puntajes",
-                    data: puntajes
-                }));
-                break;
-            case "postPuntaje":
-                const nuevoPuntaje = new puntajesModel({
-                    nombre: body.name,
-                    puntaje: body.value
-                });
-                await nuevoPuntaje.save();
-                puntajes = await puntajesModel.find();
-                wss.clients.forEach(client => {
-                    if (!puntajes) {
-                        socket.send(JSON.stringify({
-                            key: "puntajes",
-                            data: ["no hay puntaje"]
-                        }));
-                    }
-                    if (puntajes.length > 0) {
-                        puntajes = puntajes.sort((a, b) => b.puntaje - a.puntaje);
-                    }
-                    if (client.readyState == WebSocket.OPEN) {
-                        client.send(
-                            JSON.stringify({
-                                key: "newPuntaje",
-                                data: puntajes
-                            })
-                          );
-                    }
-                });
-                break;
+  socket.onmessage = async (event) => {
+    const data = event.data;
+    const parsedData: EventAttributes = JSON.parse(data.toString());
+    const action = parsedData.action;
+    const body = parsedData.body;
+    let puntajes: IPuntaje[];
+    switch (action) {
+      case "getPuntajes":
+        puntajes = await puntajesModel.find();
+        if (puntajes.length === 0) {
+          socket.send(
+            JSON.stringify({
+              key: "puntajes",
+              data: ["no hay puntajes"],
+            })
+          );
+        } else {
+          puntajes = puntajes.sort((a, b) => b.puntaje - a.puntaje);
+          socket.send(
+            JSON.stringify({
+              key: "puntajes",
+              data: puntajes,
+            })
+          );
         }
+        console.log("aqui");
+        break;
+      case "postPuntaje":
+        const nuevoPuntaje = new puntajesModel({
+          nombre: body.name,
+          puntaje: body.value,
+        });
+        await nuevoPuntaje.save();
+        puntajes = await puntajesModel.find();
+        puntajes = puntajes.sort((a, b) => b.puntaje - a.puntaje);
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(
+              JSON.stringify({
+                key: "newPuntaje",
+                data: puntajes,
+              })
+            );
+          }
+        });
+        console.log("no");
+        break;
+      default:
+        console.log("Fallo en el bucle");
+        break;
     }
-
-})
+  };
+});
 
 const PORT = process.env.PORT;
 
 connectToDatabase().then(() => {
-    server.listen(PORT, () => signale.success(`servidor escuchando en el puerto ${PORT}`))
-})
+  server.listen(PORT, () =>
+    signale.success(`servidor escuchando en el puerto ${PORT}`)
+  );
+});
